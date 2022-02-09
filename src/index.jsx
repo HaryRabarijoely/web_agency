ccd23cadecd61df086943a8ac54aa3e7c9603a7e
hary rabarijoely <rabonia@hotmail.com>
2022-02-09 23:24:27 +0100

    first ajust css

--- src/index.jsx
+++ src/index.jsx
@@ -1,45 +1,14 @@
-import React, { useCallback, useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Works from './pages/Works';
-import Navbar from './components/Navbar';
-import Theme from './Theme';
+import './index.css';
+import App from './App';
 
-const App = () => {
 
-    const [theme, setTheme] = useState(localStorage.getItem("theme"));
 
-    const ToggleTheme = useCallback(function () {
-        setTheme(t => t === 'light' ? 'dark' : 'light')
-    }, []);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-    const currentTheme = theme === 'light' ? Theme.light : Theme.dark;
-    const button = theme === 'light' ? 'Dark mode' : 'Light mode'
-
-    localStorage.setItem('theme', theme);
-
-    return (
-        <Router>
-            <button onClick={ToggleTheme}>{button}</button>
-            <Navbar />
-            <main style={currentTheme}>
-                
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                        
-                    
-                    <Route path="/About" element={<About />} />
-                        
-                    
-                    <Route path="/Works" element={<Works />} />
-                        
-                    
-                </Routes>
-            </main>
-        </Router>
-    );
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
